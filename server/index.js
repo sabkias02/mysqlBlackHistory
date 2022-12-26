821277b1908859a6dc2a8e5230a5b60d1c90093a
Sabrina <sabkias@hotmail.com>
2022-12-26 16:32:58 -0500

    cors

--- server/index.js
+++ server/index.js
@@ -1,13 +1,44 @@
 const express = require("express");
-const mysql = require("mysql");
-
+const mysql = require("mysql2");
+const cors = require("cors");
 const App = express();
+
 const db = mysql.createConnection({
-  host:
+  host: "localhost",
+  user: "root",
+  password: "100251Ali",
+  database: "newProposal",
+});
 
+App.use(express.json());
+App.use(cors());
+App.get("/", (req, res) => {
+  res.json("this is the backend ");
+});
 
-})
+App.get("/proposals", (req, res) => {
+  const q = "SELECT * FROM proposals";
+  db.query(q, (err, data) => {
+    if (err) {
+      return res.json(err);
+    } else {
+      return res.json(data);
+    }
+  });
+});
 
-App.listen(3306, () => {
+App.post("/proposals", (req, res) => {
+  const q = "INSERT INTO proposals (name,title,description,upload) VALUES (?)";
+  const VALUES = ["kias", "til", "QWERTYQWERTYQWERTY", "UPLOAD.PNG"];
+  db.query(q, [VALUES], (err, data) => {
+    if (err) {
+      return res.json(err);
+    } else {
+      return res.json(data);
+    }
+  });
+});
+
+App.listen(8003, () => {
   console.log("hey , your server is running ");
 });
